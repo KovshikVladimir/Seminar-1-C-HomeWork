d0975dc98c4c7863764b24ba368cf2cccadb43a9
KovshikVladimir <SwidishOne@gmail.com>
2022-06-27 18:18:12 +0300

    Seminar6_C#_HomeWork

---
+++ Seminar6_HomeWork/Program.cs
@@ -0,0 +1,50 @@
+﻿// // Задача 41: Пользователь вводит с клавиатуры числа через запятую. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+
+// int result = 0;
+// int i = 0;
+// int[] inputs = new int[99];
+// int number = InputNum("Введите число:");
+// Console.WriteLine("Чтобы выйти, наберите: 999");
+// while (number != 999)
+// {
+//     inputs[i] = number;
+//     number = InputNum("Введите число:");
+//     i++;
+//     if (number > 0)
+//     {
+//         result++;
+//     }
+// }
+
+//     PrintArray(inputs);
+//     Console.WriteLine();
+//     Console.WriteLine($"{result} - количество чисел больше 0");
+
+// void PrintArray(int[] array)
+
+// {
+//     for (int j = 0; j < i; j++)
+//     {
+//         Console.Write(array[j] + " ");
+//     }
+// }
+int InputNum(string msg)
+
+{
+    Console.WriteLine(msg);
+    return Convert.ToInt16(Console.ReadLine());
+}
+
+
+// Задача 43. Напишите программу, которая найдёт точку пересечения двух прямых, заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
+
+int k1 = InputNum("Введите k1:");
+int b1 = InputNum("Введите b1:");
+int k2 = InputNum("Введите k2:");
+int b2 = InputNum("Введите b2:");
+
+int firstCoodrdinate = (-b2 + b1) / (k2 - k1);
+int secondCoordinate = k1 * firstCoodrdinate + b1;
+
+Console.WriteLine();
+Console.Write($"M [{firstCoodrdinate};{secondCoordinate}] - точка пересечения прямых A и B.");
